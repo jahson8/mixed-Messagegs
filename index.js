e5b9bc5309fa071e5f7a02e35efba697d23013f0
J Mill <jahson8@hotmail.com>
2020-12-06 15:13:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const data = [
+    [
+        "How’s life been treating you?",
+        "What’s cracking?",
+        "What's up?",
+        "Hey Good to see you",
+        "How’s it going?",
+        "What’s cracking?",
+        "Hey, long time no see.",
+        "It’s been ages!"
+    ],
+    [
+        
+    ],
+    []
+];
